edf50b43a575ab9530ddd6bbedb7e580deecf316
Aleksa Piperski <mr.alexpiperski@gmail.com>
2025-07-25 13:38:26 +0200

    Poke fetching added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [pokeScore, setPokeScore] = useState([]);
+  const [scoreBoard, setScoreBoard] = useState(0);
+
+  useEffect(() => {
+    const names = ["pikachu", "bulbasaur", "charmander"];
+
+    Promise.all(
+      names.map((name) =>
+        fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then((res) =>
+          res.json()
+        )
+      )
+    ).then((data) => setPokemons(data));
+  }, []);
+
+  const onClickHandler = (e, id) => {
+    e.preventDefault();
+    scoreChecker(id);
+    console.log("pokemons : ", pokemons);
+    console.log("pokemScore : ", pokeScore);
+  };
+
+  const scoreChecker = (id) => {
+    if (pokeScore.includes(id)) {
+      console.log("IT exists");
+    } else {
+      setPokeScore((prev) => [...prev, id]);
+      setScoreBoard((prev) => prev + 1);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Pokémon</h1>
+      <h3>Score: {scoreBoard}</h3>
+      {pokemons.map((p) => (
+        <div onClick={(e) => onClickHandler(e, p.id)} key={p.id}>
+          <h2>{p.name}</h2>
+          <img src={p.sprites.front_default} alt={p.name} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default App
+export default App;
